dc96b3856db57017b2cab0de99c78ce16331a95e
alex <Asmodians@hotmail.com>
2023-03-31 14:08:44 +0300

    1

--- setup.py
+++ setup.py
@@ -2,20 +2,19 @@
 from setuptools import setup, find_namespace_packages
 
 setup(
-    name='clean folder',
+    name='clean folder insight',
     version='0.0.1',
     description='Script, which sort the folder',
     author='Alex',
     author_email='Asmo@hotmail.com',
     url='https://github.com/AleksandrRevuka/clean_folder.git',
     license='MIT',
-    classifiers=[
-        "Programming Language :: Paython :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent"],
+# classifiers = [
+#     "Programming Language :: Python :: 3",
+#     "License :: OSI Approved :: MIT License",
+#     "Operating System :: OS Independent",],
     packages=find_namespace_packages(),
-    data_files=[
-        ('clean_folder', ['clean_folder/README.md', 'clean_folder/LICENSE'])],
+    # data_files=[('clean_folder', [''])],
     include_package_data=True,
     entry_points={'console_scripts': ['clean-folder=clean_folder.sort:main']}
 )
