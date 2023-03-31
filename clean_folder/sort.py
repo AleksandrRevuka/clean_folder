402389987968e3f744810c863a963be05a1e682f
alex <Asmodians@hotmail.com>
2023-03-31 13:23:39 +0300

    sort cange

--- clean_folder/sort.py
+++ clean_folder/sort.py
@@ -4,7 +4,6 @@ import sys
 import shutil
 
 from typing import NamedTuple, List, Dict, Tuple
-# from prettytable import PrettyTable
 
 
 PUNCTUATION = "#$%&'()*+,-/:;<=>?@[\\]^_`{|}~"  # without "."
