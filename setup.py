06e77ad73f0ee95210a8fdeb19625cabaecc240a
alex <Asmodians@hotmail.com>
2023-03-31 20:16:36 +0300

    _4

--- setup.py
+++ setup.py
@@ -9,10 +9,10 @@ setup(
     author_email='Asmo@hotmail.com',
     url='https://github.com/AleksandrRevuka/clean_folder_Insighr.git',
     license='MIT',
-# classifiers = [
-#     "Programming Language :: Python :: 3",
-#     "License :: OSI Approved :: MIT License",
-#     "Operating System :: OS Independent",],
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",],
     packages=find_namespace_packages(),
     # data_files=[('clean_folder', [''])],
     include_package_data=True,
